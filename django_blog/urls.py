f0f8a261de8186b5d6631e1ec70caffecd964a4e
ValentinaLV <tina.lysenok@gmail.com>
2020-01-09 00:39:49 +0200

    create project visualization

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,4 +1,4 @@
-"""untitled2 URL Configuration
+"""django_blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ]
