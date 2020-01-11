ca3f62289515aef1312624b77684b5bc5818c206
ValentinaLV <tina.lysenok@gmail.com>
2020-01-11 16:44:36 +0200

    implement search of posts logic

--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,20 @@ from .forms import TagForm, PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
+from django.db.models import Q
+
 
 def posts(request):
-    posts_ = Post.objects.all()
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts_ = Post.objects.filter(
+            Q(title__icontains=search_query) |
+            Q(body__icontains=search_query)
+        )
+    else:
+        posts_ = Post.objects.all()
+
     posts_per_page = 4
     paginator = Paginator(posts_, posts_per_page)
 
